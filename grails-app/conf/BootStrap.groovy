b10360615594d79bf7f0db3826e0218831f11610
a-chancey <abchiptop@live.com>
2014-09-05 09:56:26 -0400

    Commented out the bootstrap, configured the app to use mysql database.
    Run this command in mysql
    CREATE database blogsite;
    
    GRANT ALL PRIVILEGES ON blogsite.* TO 'blogsite'@'localhost'
    IDENTIFIED BY 'blogsite' WITH GRANT OPTION;

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,65 +5,65 @@ class BootStrap {
 
 	def init = { servletContext ->
 
-		new Post(title:"First Grails Project",
-		teaser:"Clearing out the clutter",
-		content:"The full content of the article",
-		published:true).save()
-
-		new Post(title:"Getting Ready for Go Live",
-		teaser:"The follow up on some real world issues.",
-		content:"The full content of the article",
-		published:false).save()
-
-		new Post(title:"Yet another post",
-		teaser:"had to generate this",
-		content:"The full content of the article",
-		published:true).save()
-
-		new Post(title:"A post again?",
-		teaser:"well at least this will go quick",
-		content:"The full content of the article",
-		published:false).save()
-
-		new Post(title:"Time management",
-		teaser:"how to save time writing",
-		content:"The full content of the article",
-		published:true).save()
-
-		new Post(title:"Who is the target",
-		teaser:"targets of your business",
-		content:"The full content of the article",
-		published:false).save()
-
-		new Post(title:"Bob Funland's arcade",
-		teaser:"come to bob's for fun",
-		content:"The full content of the article",
-		published:true).save()
-
-		new Post(title:"The other guys",
-		teaser:"do they have the answers",
-		content:"The full content of the article",
-		published:false).save()
-
-		new Post(title:"Friends and enemies",
-		teaser:"some guy wrote all this",
-		content:"The full content of the article",
-		published:true).save()
-
-		new Post(title:"last test post of the millenium",
-		teaser:"it's all over man.",
-		content:"The full content of the article",
-		published:false).save()
-		
-		new Post(title:"Ignore that last guy, this is the last post",
-			teaser:"it's finally over man",
-			content:"The full content of the article is a lot cooler",
-			published:false).save()
-
-		new User(username:"me",
-		password:"password",
-		firstName:"A",
-		lastName:"Developer").save()
+//		new Post(title:"First Grails Project",
+//		teaser:"Clearing out the clutter",
+//		content:"The full content of the article",
+//		published:true).save()
+//
+//		new Post(title:"Getting Ready for Go Live",
+//		teaser:"The follow up on some real world issues.",
+//		content:"The full content of the article",
+//		published:false).save()
+//
+//		new Post(title:"Yet another post",
+//		teaser:"had to generate this",
+//		content:"The full content of the article",
+//		published:true).save()
+//
+//		new Post(title:"A post again?",
+//		teaser:"well at least this will go quick",
+//		content:"The full content of the article",
+//		published:false).save()
+//
+//		new Post(title:"Time management",
+//		teaser:"how to save time writing",
+//		content:"The full content of the article",
+//		published:true).save()
+//
+//		new Post(title:"Who is the target",
+//		teaser:"targets of your business",
+//		content:"The full content of the article",
+//		published:false).save()
+//
+//		new Post(title:"Bob Funland's arcade",
+//		teaser:"come to bob's for fun",
+//		content:"The full content of the article",
+//		published:true).save()
+//
+//		new Post(title:"The other guys",
+//		teaser:"do they have the answers",
+//		content:"The full content of the article",
+//		published:false).save()
+//
+//		new Post(title:"Friends and enemies",
+//		teaser:"some guy wrote all this",
+//		content:"The full content of the article",
+//		published:true).save()
+//
+//		new Post(title:"last test post of the millenium",
+//		teaser:"it's all over man.",
+//		content:"The full content of the article",
+//		published:false).save()
+//		
+//		new Post(title:"Ignore that last guy, this is the last post",
+//			teaser:"it's finally over man",
+//			content:"The full content of the article is a lot cooler",
+//			published:false).save()
+//
+//		new User(username:"me",
+//		password:"password",
+//		firstName:"A",
+//		lastName:"Developer").save()
 	}
 	def destroy = {
 	}
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,11 +17,21 @@ hibernate {
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
+//        dataSource {
+//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+//        }
+		dataSource {
+			pooled = true
+			dbCreate = "update"
+			url = 'jdbc:mysql://localhost:3306/blogsite'
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+			username = "root"
+			password = "root"
+		}
+    }		
+    
     test {
         dataSource {
             dbCreate = "update"
@@ -30,28 +40,13 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
-        }
+			pooled = true
+			dbCreate = "update"
+			url = 'jdbc:mysql://localhost:3306/blogsite'
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+			username = "root"
+			password = "root"
+		}
     }
 }
